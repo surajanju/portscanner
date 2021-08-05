a8779aac3bd5bdddffcb6e6156f8ad93c6565274
surajanju <53549418+surajanju@users.noreply.github.com>
2021-08-05 09:27:48 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import socket
+from IPy import IP
+
+
+def scan(target):
+    converted_ip = check_ip(target)
+    print('\n' + '[*_*] Scanning Target' + str(target))
+    for port in range(24, 443):
+        scan_port(converted_ip, port)
+
+
+def check_ip(ip):
+    try:
+        IP(ip)
+        return (ip)
+    except ValueError:
+        return socket.gethostbyname(ip)
+
+
+def get_banner(s):
+    return s.recv(1024)
+
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.45)
+        sock.connect((ipaddress, port))
+        try:
+            banner = get_banner(sock)
+            print('[+] OPEN Port' + str(port) + ':' + str(banner.decode() .strip('\n')))
+        except:
+            print('[+] Open Port' + str(port))
+    except:
+        pass
+
+
+targets = input('[+] Enter Target/s (,):')
+if ',' in targets:
+    for ip_add in targets.split(','):
+        scan(ip_add.strip(' '))
+else:
+    scan(targets)
+
